f3282686cee02ae1776dcf83ae015e938c177c72
AnimalRationale <wojciech.mardyla@gmail.com>
2015-11-26 23:00:51 +0000

    Encoded in UTF8, removed not used code, cleanup in tabs and spaces.

--- noteuploader01.php
+++ noteuploader01.php
@@ -1,20 +1,19 @@
 <?php
 
-   $jsonurl = $_REQUEST['json'];    
-   // $json = urldecode($jsonurl);
+   $jsonurl = $_REQUEST['json'];
    $json = html_entity_decode($jsonurl);
-   $json = str_replace("\\","", $json);  
-   $data = json_decode($json, true);   
-    
+   $json = str_replace("\\","", $json);
+   $data = json_decode($json, true);
+
    if (isset($data['check_uploader'])) {
-        print("Uploader OK");        
+        print("Uploader OK");
      }
 
    $server_pass = "Blogtrotter.v01.236";
-   $pass = "No-pass";   
+   $pass = "No-pass";
 
    if (isset($data['pass'])) {
-        $pass = $data['pass'];        
+        $pass = $data['pass'];
      }
 
    if ($pass != $server_pass) {
@@ -23,33 +22,22 @@
      }
 
    $base = $data['photo'];
-   // print(" BAZA1: ".$base);
    $base = str_replace("\\","", $base);
-   // print(" BAZA2: ".$base);
-   // if (base64_encode(base64_decode($base)) === $base){
-   // echo ' $base is valid';
-   // } else {
-   // echo ' $base is NOT valid';
-   // }
 
    $photo_filename = $data['note_id'].".jpg";
-   
+
    $buffer = base64_decode($base);
-   file_put_contents($photo_filename, $buffer);   
-   
-   $filename = $data['note_id'].$data['author_id'].".json";   
-   print(" :: ".$filename);   
-   
+   file_put_contents($photo_filename, $buffer);
+
+   $filename = $data['note_id'].$data['author_id'].".json";
+   print(" :: ".$filename);
+
    $data['photo'] = $photo_filename;
    $json = json_encode($data);
 
    if ($data != null) { /* sanity check */
      header('Content-type: application/json; charset=utf-8');
-     // $file = fopen($filename,'w+');
-     // $json = str_replace("\\","", $json);
      file_put_contents($filename, $json);
-     // fwrite($file, $json);
-     // fclose($file);
    } else {
-     print("File write error"); // handle error 
+     print("File write error"); // handle error
    }
--- show.php
+++ show.php
@@ -1,25 +1,23 @@
-<?PHP
-  $jsons = array();  
+﻿<?PHP
+  $jsons = array();
   $jsons_path = getcwd();
-  // echo "Dir: ".$jsons_path."</br>";
   $dir = opendir($jsons_path);
-	while ($file = readdir($dir)) {
-	 	if (eregi("\.json",$file)) {
-	 		$jsons[] = $file; 
-	 	}
-	}
-	sort($jsons);
+    while ($file = readdir($dir)) {
+         if (eregi("\.json",$file)) {
+             $jsons[] = $file;
+         }
+    }
+    sort($jsons);
 
-  // echo "<meta http-equiv='refresh' content='3' >"
   echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
     <link rel='stylesheet' type='text/css' href='show.css' /><html><body>";
 
   foreach ($jsons as $json){
-    $data = json_decode(file_get_contents($json), true);    
+    $data = json_decode(file_get_contents($json), true);
     echo "<div class='title'>".$data['title']."</div></br><div class='date'>".$data['date'].", ".$data['time']."</br>";
     echo "<div class='author'>".$data['author']."</div></br>";
     echo "<div class='note'>".$data['note']."</div></br>";
-    if ($data['rotation'] == "yes") {      
+    if ($data['rotation'] == "yes") {
       echo "</br></br></br></br></br><img src='".$data['photo']."' style='-webkit-transform: rotate(90deg)'; alt='note_photo' />";
     } else {
         echo "<img src='".$data['photo']."'; alt='note_photo' />";
